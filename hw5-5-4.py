f2487ba04d7e1c1abab3aa1569bc16f840ec8a03
p22cobrien <p22cobrien@fairifeldprep.org>
2021-11-02 16:14:22 -0400

    submit hw5-5-4.py

--- hw5-5-4.py
+++ hw5-5-4.py
@@ -1,8 +1,8 @@
 # Author: CMOB 11/2/2021
 
-word_1 = input("Please enter the first word. ")
-word_2 = input("Please enter the second word. ")
-word_3 = input("Please enter the thrid word. ")
+word_1 = str(input("Please enter the first word. "))
+word_2 = str(input("Please enter the second word. "))
+word_3 = str(input("Please enter the thrid word. "))
 
 a = str.capitalize(word_1)
 b = str.capitalize(word_2)
@@ -11,4 +11,18 @@ c = str.capitalize(word_3)
 if a > b and a > c:
     if b > c:
         print(word_1, word_2, word_3)
-elif a > b
+elif a > b and a < c:
+    if b < c:
+        print(word_1, word_3, word_2)
+elif a < b and a < c:
+    if b > c:
+        print(word_2, word_3, word_1)
+elif a < b and a > c:
+    if b > c:
+        print(word_2, word_1, word_3)
+elif a < b and a < c:
+    if b < c:
+        print(word_3, word_2, word_1)
+elif a > b and a < c:
+    if b < c:
+        print(word_3, word_1, word_2)
